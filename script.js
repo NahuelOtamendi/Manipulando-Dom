f1b3168171d6447dc2a9e1f4317be93fa9672741
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-05-12 18:51:57 -0300

    se agrego funcionalidad de crear nueva tarjeta a js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const btn = document.querySelector("[data-form-btn]");
 const createTask =  (evento) =>{
     evento.preventDefault();
-    const input = document.querySelector("[data-form-imput]")
-    console.log(input.value)
+    const input = document.querySelector("[data-form-imput]");
+    const value = input.value;
+    const task = document.querySelector("[data-task]");
+    input.value = "";
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content; 
+  console.log(task)
   }
 
 btn.addEventListener("click", createTask);
