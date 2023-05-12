0da3f8378722d26af1e43cb8f5f8b3eea616a6cd
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-05-12 19:13:07 -0300

    funcionalidad crear tarjetas terminada

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const createTask =  (evento) =>{
     evento.preventDefault();
     const input = document.querySelector("[data-form-imput]");
     const value = input.value;
-    const task = document.querySelector("[data-task]");
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
     input.value = "";
     const content = `<div>
     <i class="far fa-check-square icon"></i>
@@ -11,6 +13,7 @@ const createTask =  (evento) =>{
   </div>
   <i class="fas fa-trash-alt trashIcon icon"></i>`;
     task.innerHTML = content; 
+    list.appendChild(task)
   console.log(task)
   }
 
