b51499003a5b3fc72bd89db3583ef54afa985f33
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-05-15 06:48:47 -0300

    Se agrego funcionalidad de eliminar tareas y se separo el codigo por componentes, Clase 04 completada

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
-( () => {
+  import checkComplete from "./componentes/checkComplete.js";
+  import deleteIcon from "./componentes/deletIcon.js";
 
-const btn = document.querySelector("[data-form-btn]");
-const createTask =  (evento) =>{
+  const btn = document.querySelector('[data-form-btn]');
+
+  const createTask = (evento) => {
     evento.preventDefault();
-    const input = document.querySelector("[data-form-imput]");
+    const input = document.querySelector('[data-form-input]');
     const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    input.value = "";
-    const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplete())
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+   
+    const taskContent = document.createElement('div');
+
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
     titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    const content = 
-    `${checkComplete()}
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    // task.innerHTML = content; 
+    
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
-  }
+  };
 
-btn.addEventListener("click", createTask);
+  btn.addEventListener('click', createTask);
 
-const checkComplete = () =>{
-  const i = document.createElement("i")
-  i.classList.add("far", "fa-check-square", "icon")
-  i.addEventListener("click", completeTask);
-  return i;
-}
-
-const completeTask = (evento) =>{
-  const element = evento.target;
-  element.classList.toggle("fas");
-  element.classList.toggle("completeIcon")
-  element.classList.toggle("far");
-}
-
-}) ();
